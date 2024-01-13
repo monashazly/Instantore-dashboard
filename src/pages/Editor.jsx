879f5f882353b1e05965ab77423c63db42e4dae1
Mona Shazly <monashazly@Monas-MacBook-Air.local>
2024-01-13 15:01:50 +0200

    editor

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import {
+  HtmlEditor,
+  Image,
+  Inject,
+  Link,
+  QuickToolbar,
+  RichTextEditorComponent,
+  Toolbar,
+} from "@syncfusion/ej2-react-richtexteditor";
+
+import { Header } from "../components";
 
 const Editor = () => {
   return (
-    <div>
-      
+    <div className="mx-2 mt-20 md:m-10 p-2 md:p-5 bg-white rounded-3xl">
+      <Header category="App" title="Editor" />
+      <RichTextEditorComponent>
+        <Inject services={[HtmlEditor, Image, Link, QuickToolbar, Toolbar]} />
+      </RichTextEditorComponent>
     </div>
-  )
-}
+  );
+};
 
-export default Editor
+export default Editor;
